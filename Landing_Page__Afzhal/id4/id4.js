43bddb1d2f12904eddf6c77254bdfb6a5b617b06
Afzhal-ahmed-s <writetoafzhalahmeds@gmail.com>
2022-06-15 20:39:20 +0530

    first

---
+++ Landing_Page__Afzhal/id4/id4.js
@@ -0,0 +1,30 @@
+// For first card
+document.querySelector("#one").addEventListener("click",redirect_no1);
+
+function redirect_no1(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For second card
+document.querySelector("#two").addEventListener("click",redirect_no2);
+
+function redirect_no2(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For third card
+document.querySelector("#three").addEventListener("click",redirect_no3);
+
+function redirect_no3(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For fourth card
+document.querySelector("#four").addEventListener("click",redirect_no4);
+
+function redirect_no4(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For fifth card
+document.querySelector("#five").addEventListener("click",redirect_no5);
+
+function redirect_no5(){
+    location.href="./dummy_link_page_demo.html";
+}
