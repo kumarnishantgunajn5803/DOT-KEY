9954dde87f3795032d4fef21c5b022ef3ae99122
Afzhal-ahmed-s <writetoafzhalahmeds@gmail.com>
2022-06-16 16:51:19 +0530

    commit

---
+++ Landing_Page__Afzhal/id 9/id9.js
@@ -0,0 +1,25 @@
+// For first card
+document.querySelector("#one_id9").addEventListener("click",redirect_no1_id9);
+
+function redirect_no1_id9(){
+    location.href="./dummy_link_page_demo.html";
+}
+
+// For second card
+document.querySelector("#two_id9").addEventListener("click",redirect_no2_id9);
+
+function redirect_no2_id9(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For third card
+document.querySelector("#three_id9").addEventListener("click",redirect_no3_id9);
+
+function redirect_no3_id9(){
+    location.href="./dummy_link_page_demo.html";
+}
+// For fourth card
+document.querySelector("#four_id9").addEventListener("click",redirect_no4_id9);
+
+function redirect_no4_id9(){
+    location.href="./dummy_link_page_demo.html";
+}
