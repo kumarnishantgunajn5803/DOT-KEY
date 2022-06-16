96df5c4c2342bae5b81f563a486a5cbde31fffd4
Afzhal-ahmed-s <writetoafzhalahmeds@gmail.com>
2022-06-16 09:55:46 +0530

    commit

--- Landing_Page__Afzhal/id12/id12.js
+++ Landing_Page__Afzhal/id12/id12.js
@@ -1,6 +1,6 @@
-document.querySelector("#one").addEventListener("click",redirect_no1);
+document.querySelector("#one_id12").addEventListener("click",redirect_no1_id12);
 
-function redirect_no1(){
+function redirect_no1_id12(){
     location.href="./dummy_link_page_demo.html";
 }
 
--- Landing_Page__Afzhal/id4/id4.js
+++ Landing_Page__Afzhal/id4/id4.js
@@ -1,30 +1,30 @@
 // For first card
-document.querySelector("#one").addEventListener("click",redirect_no1);
+document.querySelector("#one_id4").addEventListener("click",redirect_no1_id4);
 
-function redirect_no1(){
+function redirect_no1_id4(){
     location.href="./dummy_link_page_demo.html";
 }
 // For second card
-document.querySelector("#two").addEventListener("click",redirect_no2);
+document.querySelector("#two_id4").addEventListener("click",redirect_no2_id4);
 
-function redirect_no2(){
+function redirect_no2_id4(){
     location.href="./dummy_link_page_demo.html";
 }
 // For third card
-document.querySelector("#three").addEventListener("click",redirect_no3);
+document.querySelector("#three_id4").addEventListener("click",redirect_no3_id4);
 
-function redirect_no3(){
+function redirect_no3_id4(){
     location.href="./dummy_link_page_demo.html";
 }
 // For fourth card
-document.querySelector("#four").addEventListener("click",redirect_no4);
+document.querySelector("#four_id4").addEventListener("click",redirect_no4_id4);
 
-function redirect_no4(){
+function redirect_no4_id4(){
     location.href="./dummy_link_page_demo.html";
 }
 // For fifth card
-document.querySelector("#five").addEventListener("click",redirect_no5);
+document.querySelector("#five_id4").addEventListener("click",redirect_no5_id4);
 
-function redirect_no5(){
+function redirect_no5_id4(){
     location.href="./dummy_link_page_demo.html";
 }
