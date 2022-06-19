4ff05bec3150eb517c47403855fd51b50df391e2
Afzhal-ahmed-s <writetoafzhalahmeds@gmail.com>
2022-06-19 12:38:31 +0530

    search bar front end fixed

--- common/navbar.js
+++ common/navbar.js
@@ -2,3 +2,19 @@ function afterclick() {
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
 }
+
+document.getElementsByClassName('searchBtn')[0].addEventListener('mouseover', () => {
+    document.getElementsByClassName('searchText')[0].style.display = 'block';
+});
+
+document.getElementsByClassName('searchText')[0].addEventListener('mouseover', () => {
+    document.getElementsByClassName('searchText')[0].style.display = 'block';
+});
+
+document.getElementsByClassName('searchBtn')[0].addEventListener('mouseout', () => {
+    document.getElementsByClassName('searchText')[0].style.display = 'none';
+});
+
+document.getElementsByClassName('searchText')[0].addEventListener('mouseout', () => {
+    document.getElementsByClassName('searchText')[0].style.display = 'none';
+});
